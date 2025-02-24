0786d31e2908fcc083be3cb341cb79e84dc88ece
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-02-24 16:03:58 -0500

    Add Windows-specific linking and entry point support
    Enabled `winmm` library linking in CMake for Windows builds and added a `WinMain` entry point in `main.cpp` for proper application startup on the platform. These changes ensure compatibility and correct execution on Windows systems.

--- main.cpp
+++ main.cpp
@@ -14,3 +14,13 @@ int main()
 
     CloseWindow();
 }
+
+
+#ifdef _WIN32
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    return main(__argc, __argv);
+}
+#endif
