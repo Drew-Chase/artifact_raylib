13efcb7ba1ac6d20e04c3887371b92c4a2a98c6c
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-02-25 11:00:52 -0500

    Add new resource retrieval methods to ResourceManager
    Expanded ResourceManager to support specialized resource retrieval methods for textures, JSON, text, and binary files. Introduced a method to determine a resource type based on a file path. These changes enhance flexibility and modularity in resource handling.

--- include/resource_manager.h
+++ include/resource_manager.h
@@ -1,10 +1,18 @@
 #pragma once
+#include <string>
+
+#include "resource.h"
 
 namespace artifact
 {
     class ResourceManager
     {
     public:
-        static
+        static Resource get_resource(const char *identifier, const ResourceType &type);
+        static Resource get_texture(const char *identifier);
+        static Resource get_json(const char *identifier);
+        static Resource get_text(const char *identifier);
+        static Resource get_binary(const char *identifier);
+        static ResourceType get_type(const std::string &path);
     };
 } // namespace artifact
