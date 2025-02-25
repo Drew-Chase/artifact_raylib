e218d71bbab5227746dcd3194e5baacc9ec48baf
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-02-25 16:06:30 -0500

    Refactor Game class to use singleton pattern.
    Introduced the singleton pattern for the Game class by adding a static instance and private constructors. Deleted copy constructor and assignment operator to enforce singleton behavior. Also updated the run method and added a getter for StageManager.

--- include/game.h
+++ include/game.h
@@ -1,10 +1,31 @@
 #pragma once
+#include <cstdarg>
+
+
+#include "stages/stage_manager.h"
 
 namespace artifact
 {
     class Game
     {
+        StageManager *manager;
+        Game(); // Keep constructor private
+        ~Game(); // Keep destructor private
+
+        // Delete copy constructor and assignment operator
+        Game(const Game &) = delete;
+        Game &operator=(const Game &) = delete;
+
+        // Add static instance
+        static Game *instance;
+        static void SpdLoggerCallback(int msgType, const char* message, va_list args);
+
     public:
+        // Get singleton instance
+        static Game *getInstance();
+
+        // Replace run with static method that uses singleton
         static void run();
+        StageManager *getManager() const { return manager; }
     };
 } // namespace artifact
