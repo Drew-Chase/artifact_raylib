f078b8291a2497cd3393a89193bc5c0ead961ade
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-02-25 16:07:16 -0500

    Add StageManager class with stage management functionality
    Introduce `StageManager` to handle stage lifecycle operations including loading and retrieving the current stage. The implementation utilizes a pointer to manage the current stage and ensures proper cleanup in the destructor.

--- src/stages/stage_manager.cpp
+++ src/stages/stage_manager.cpp
@@ -1,1 +1,11 @@
 #include "../include/stages/stage_manager.h"
+artifact::StageManager::StageManager() {}
+artifact::StageManager::~StageManager() { delete this->current_stage; }
+artifact::Stage *artifact::StageManager::loadStage(const char *identifier)
+{
+    const auto stage = new Stage(identifier);
+    stage->update(true);
+    stage->draw(true);
+    this->current_stage = stage;
+    return stage;
+}
