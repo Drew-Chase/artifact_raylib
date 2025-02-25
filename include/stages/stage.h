a78bfbe013aa620003b6c988fedc1c280879e18c
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-02-25 16:07:49 -0500

    Refactor `Stage` class and add utility methods.
    Replaced deleted destructor with a default one for flexibility. Added static constant `TITLE_SCREEN` and overloaded `draw` and `update` methods for enhanced functionality. Simplified constructor initialization for better readability.

--- include/stages/stage.h
+++ include/stages/stage.h
@@ -7,9 +7,16 @@ namespace artifact
         const char *identifier;
 
     public:
-        explicit Stage(const char *identifier) { this->identifier = identifier; }
+        static constexpr const char *TITLE_SCREEN = "title_screen";
 
-        ~Stage() = delete;
+
+        explicit Stage(const char *identifier) : identifier(identifier) {}
+
+        ~Stage() = default;
+        void draw() const { draw(false); }
+        void draw(bool first_draw) const;
+        void update() const { update(false); }
+        void update(bool first_update) const;
 
         [[nodiscard]] const char *getIdentifier() const { return this->identifier; }
     };
