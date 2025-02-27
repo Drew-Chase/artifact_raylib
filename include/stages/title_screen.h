fbe43463791cc6165dac9ad785f80b2dbecc1a9c
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-02-27 15:16:52 -0500

    Add background image rendering to TitleScreen
    Introduced a `background_image` property to the `TitleScreen` class and implemented its loading in the `startup` method. Updated the `draw` method to display the background image, enhancing the visual presentation of the title screen.

--- include/stages/title_screen.h
+++ include/stages/title_screen.h
@@ -1,14 +1,20 @@
 #pragma once
+#include <raylib.h>
+
+
 #include "menu_stage.h"
 
 namespace artifact
 {
     class TitleScreen : public MenuStage
     {
+    private:
+        Texture2D background_image;
     public:
         explicit TitleScreen() : MenuStage("title_screen") {}
 
         void draw() const override;
         void update() const override;
+        void startup() override;
     };
 } // namespace artifact
--- src/stages/title_screen.cpp
+++ src/stages/title_screen.cpp
@@ -8,6 +8,15 @@
 namespace artifact
 {
     size_t frames_drawn = 0;
-    void TitleScreen::draw() const { DrawText(fmt::format("Frames Drawn: {}", frames_drawn).c_str(), 20, 100, 24, WHITE); }
+    void TitleScreen::draw() const
+    {
+        DrawText(fmt::format("Frames Drawn: {}", frames_drawn).c_str(), 20, 100, 24, WHITE);
+        draw_texture_to_fill_rect(GetScreenWidth(), GetScreenHeight(), 0, 0, background_image);
+    }
     void TitleScreen::update() const { frames_drawn++; }
+    void TitleScreen::startup()
+    {
+        background_image = LoadTexture("game/texture/menus/title_screen/background.png");
+
+    }
 } // namespace artifact
