2569fa63cc8e02431c1d1e49d17c170a0e8e4f0a
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-02-27 11:05:05 -0500

    Refactor StageManager to use templated loadStage method
    Replaced the old loadStage method with a templated version enforcing type constraints for stages. This improves type safety, ensures stages derive from the Stage base class, and streamlines the stage initialization process. Removed the identifier-based loadStage implementation.

--- include/stages/stage_manager.h
+++ include/stages/stage_manager.h
@@ -1,16 +1,40 @@
 #pragma once
+#include <concepts>
+#include <type_traits>
+
+
 #include "stage.h"
+#include "title_screen.h"
 
 namespace artifact
 {
+    static auto TITLE_SCREEN = new TitleScreen();
+
     class StageManager
     {
+
         Stage *current_stage = nullptr;
 
     public:
         StageManager();
         ~StageManager();
-        Stage *loadStage(const char *identifier);
-        Stage *getCurrentStage() const { return current_stage; }
+
+        template<class T>
+            requires std::is_base_of_v<Stage, std::remove_pointer_t<T>>
+        Stage *loadStage(T stage)
+        {
+            // destroy the current stage
+            if (current_stage != nullptr)
+                current_stage->destroy();
+
+            current_stage = stage;
+            current_stage->startup();
+            current_stage->update(true);
+            current_stage->draw(true);
+            return stage;
+        }
+
+
+        [[nodiscard]] Stage *getCurrentStage() const { return current_stage; }
     };
 } // namespace artifact
