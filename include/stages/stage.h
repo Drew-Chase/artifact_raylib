ae208a65bee55ffc399ac1a2b7e676dd50910fa2
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-02-27 11:05:15 -0500

    Make Stage methods virtual and add startup/destroy hooks
    Refactored the `Stage` class to make its methods virtual, allowing for easier extensibility. Added `startup` and `destroy` hooks to provide additional lifecycle management functionality. Also updated method naming for consistency.

--- include/stages/stage.h
+++ include/stages/stage.h
@@ -7,17 +7,17 @@ namespace artifact
         const char *identifier;
 
     public:
-        static constexpr const char *TITLE_SCREEN = "title_screen";
-
-
         explicit Stage(const char *identifier) : identifier(identifier) {}
 
-        ~Stage() = default;
-        void draw() const { draw(false); }
-        void draw(bool first_draw) const;
-        void update() const { update(false); }
-        void update(bool first_update) const;
+        virtual ~Stage() = default;
 
-        [[nodiscard]] const char *getIdentifier() const { return this->identifier; }
+        virtual void draw() const { draw(false); }
+        virtual void draw(bool first_draw) const {}
+        virtual void update() const { update(false); }
+        virtual void update(bool first_update) const {}
+        virtual void startup() {}
+        virtual void destroy() {}
+
+        [[nodiscard]] const char *get_identifier() const { return this->identifier; }
     };
 } // namespace artifact
