5a77a7fc92c2f44828a43a29a6287a19bd3eeaa5
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-02-28 15:09:08 -0500

    Refactor ComponentBase interface with new methods.
    Added an `update` method and a `get_identifier` method to enhance functionality and separation of concerns. Adjusted `draw_texture_to_fill_rect` to use integer parameters for consistency. These changes improve the clarity and flexibility of the component base class.

--- include/ui/components/component_base.h
+++ include/ui/components/component_base.h
@@ -10,7 +10,9 @@ namespace artifact
     public:
         virtual ~ComponentBase() = default;
         explicit ComponentBase(const char *identifier) : identifier(identifier) {}
-        virtual void draw(int width, int height, int x, int y, int mouse_x, int mouse_y) {}
-        static void draw_texture_to_fill_rect(float width, float height, float x, float y, const Texture2D &texture);
+        virtual void draw(int width, int height, int x, int y) {}
+        virtual void update(int mouse_x, int mouse_y) {}
+        static void draw_texture_to_fill_rect(int width, int height, int x, int y, const Texture2D &texture);
+        virtual const char *get_identifier() { return identifier; }
     };
 } // namespace artifact
