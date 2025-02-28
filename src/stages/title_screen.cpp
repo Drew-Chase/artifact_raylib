7ad8b9ad2e464f2272d276ec32b4a417c56bea7a
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-02-28 15:06:12 -0500

    Refactor TitleScreen resource handling and improve rendering.
    Replaced raw texture loading with dynamic allocation to manage background_image lifecycle properly, adding a destroy function for cleanup. Updated draw logic to ensure background_image is checked before rendering. Improved text handling with fmt library for more flexible string formatting.

--- src/stages/title_screen.cpp
+++ src/stages/title_screen.cpp
@@ -1,22 +1,18 @@
-
 #include "../include/stages/title_screen.h"
-
+#include <fmt/format.h>
 #include <raylib.h>
 
-#include "game.h"
 
 namespace artifact
 {
     size_t frames_drawn = 0;
     void TitleScreen::draw() const
     {
+        if (background_image != nullptr)
+            draw_texture_to_fill_rect(GetScreenWidth(), GetScreenHeight(), 0, 0, *background_image);
         DrawText(fmt::format("Frames Drawn: {}", frames_drawn).c_str(), 20, 100, 24, WHITE);
-        draw_texture_to_fill_rect(GetScreenWidth(), GetScreenHeight(), 0, 0, background_image);
     }
     void TitleScreen::update() const { frames_drawn++; }
-    void TitleScreen::startup()
-    {
-        background_image = LoadTexture("game/texture/menus/title_screen/background.png");
-
-    }
+    void TitleScreen::startup() { background_image = new Texture2D(LoadTexture("game/texture/menus/title_screen/background.png")); }
+    void TitleScreen::destroy() { delete background_image; }
 } // namespace artifact
