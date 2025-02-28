ddf81c896c2942466775a246267e7f9083984e06
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-02-28 15:08:38 -0500

    Remove unused timer.cpp file
    The timer.cpp file was empty and no longer served a purpose. Deleting it helps clean up the codebase and reduce unnecessary clutter.

---
+++ include/ui/components/containers/container_base.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <map>
+#include <vector>
+#include <string>
+
+#include "ui/components/component_base.h"
+
+namespace artifact
+{
+    class ContainerBase : public ComponentBase
+    {
+        std::map<std::string, ComponentBase *> components;
+
+    public:
+        void draw(int width, int height, int x, int y) override;
+        void update(int mouse_x, int mouse_y) override;
+        virtual void add_component(ComponentBase *component);
+        virtual void remove_component(size_t index);
+        virtual void remove_component(const char* identifier);
+        virtual void remove_component(ComponentBase *component);
+        virtual std::vector<ComponentBase *> entries();
+    };
+} // namespace artifact
