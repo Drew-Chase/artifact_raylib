23c01887c3ee7ef9399f06372152ce60567cd776
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-02-28 15:09:32 -0500

    Add destroy method and update background_image handling
    This commit adds a `destroy` method to the `TitleScreen` class for resource cleanup and modifies `background_image` to be a pointer initialized to `nullptr`. These changes improve memory management and ensure proper handling of resources during the lifecycle of the title screen.

--- include/stages/title_screen.h
+++ include/stages/title_screen.h
@@ -1,20 +1,19 @@
 #pragma once
 #include <raylib.h>
-
-
 #include "menu_stage.h"
 
 namespace artifact
 {
     class TitleScreen : public MenuStage
     {
-    private:
-        Texture2D background_image;
+        Texture2D *background_image = nullptr;
+
     public:
         explicit TitleScreen() : MenuStage("title_screen") {}
 
         void draw() const override;
         void update() const override;
         void startup() override;
+        void destroy() override;
     };
 } // namespace artifact
