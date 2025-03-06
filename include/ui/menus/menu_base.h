d8af379d53e798a19a591be20505892fa1ee512c
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-03-06 16:23:32 -0500

    Add is_menu_in_focus method to MenuBase class
    Introduce a method to check if the current menu is in focus. This improves menu state management by leveraging the game's stage manager data.

--- include/ui/menus/menu_base.h
+++ include/ui/menus/menu_base.h
@@ -1,11 +1,17 @@
 #pragma once
+#include "game.h"
 #include "ui/components/component_base.h"
 
 namespace artifact
 {
-    class MenuBase:public ComponentBase
+    class MenuBase : public ComponentBase
     {
     public:
         explicit MenuBase(const char *identifier) : ComponentBase(identifier) {}
+        bool is_menu_in_focus() const
+        {
+            const MenuBase *menu_in_focus = Game::get_instance()->get_stage_manager()->get_current_stage()->get_menu_in_focus();
+            return menu_in_focus == this;
+        }
     };
 } // namespace artifact
