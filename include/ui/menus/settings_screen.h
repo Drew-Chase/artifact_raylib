d886280862512e858a79dbedfc83bc7d35f87e36
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-03-05 21:18:15 -0500

    Add initial skeleton for SettingsScreen class
    Introduce `SettingsScreen` class in the `artifact` namespace, derived from `MenuBase`. This sets up the basic structure for future implementation of the settings menu. No functionality is included yet.

---
+++ include/ui/menus/settings_screen.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "menu_base.h"
+
+namespace artifact
+{
+    class SettingsScreen final : public MenuBase
+    {};
+} // namespace artifact
---
+++ src/ui/menus/settings_screen.cpp
@@ -0,0 +1,5 @@
+
+#include "ui/menus/settings_screen.h"
+
+namespace artifact {
+} // artifact
