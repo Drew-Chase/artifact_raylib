9cf1fd0206363508dfa3850c2f5063ac48c09464
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-03-10 15:17:32 -0400

    Refactor Level1AStage class formatting style.
    Reformatted the Level1AStage class to adhere to consistent coding style and improved readability. Adjusted braces and spacing while preserving functionality.

--- include/stages/level_1_a_stage.h
+++ include/stages/level_1_a_stage.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "playable_stage.h"
 
-namespace artifact {
-class Level1AStage :public PlayableStage{
+namespace artifact
+{
+    class Level1AStage : public PlayableStage
+    {
 
-public:
-    void draw() const override;
-    void update(float deltaTime) const override;
-    void destroy() override;
-    void startup() override;
-};
-} // artifact
+    public:
+        void draw() const override;
+        void update(float deltaTime) const override;
+        void destroy() override;
+        void startup() override;
+    };
+} // namespace artifact
