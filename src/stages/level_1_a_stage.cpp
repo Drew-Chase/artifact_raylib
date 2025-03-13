614c8463b19ed8fcd1026b827ae9614934e65725
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-03-13 15:54:06 -0400

    Refactor Level1AStage and add initialization stubs.
    Updated namespace formatting and headers for consistency. Introduced background texture initialization and added stubs for enemy spawning and end goal setup in the startup method.

--- src/stages/level_1_a_stage.cpp
+++ src/stages/level_1_a_stage.cpp
@@ -1,15 +1,27 @@
 
-#include "../../include/stages/level_1_a_stage.h"
-
+#include "stages/level_1_a_stage.h"
 #include "entities/player_entity.h"
 
-namespace artifact {
+namespace artifact
+{
+    Level1AStage::Level1AStage() : PlayableStage("level_1a") {}
     void Level1AStage::startup()
     {
         PlayableStage::startup();
+
+        // Spawn the Player
         spawn_entity<PlayerEntity>(0, 0);
+
+        // Spawn enemies
+        // TODO: Spawn enemies
+
+        // Setup features
+        // TODO: Create end goal.
+
+        // Initialize Textures
+        set_background("game/texture/stages/level_1a/background.png");
     }
     void Level1AStage::draw() const { PlayableStage::draw(); }
-    void Level1AStage::update(const float deltaTime) const { PlayableStage::update(deltaTime); }
+    void Level1AStage::update(const float deltaTime) { PlayableStage::update(deltaTime); }
     void Level1AStage::destroy() { PlayableStage::destroy(); }
-} // artifact
+} // namespace artifact
