fda7d172fe3f40ffe873a1a4ddcdfeee5dea947c
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-03-15 03:00:48 -0400

    Add new colliders and adjust existing ones in Level 1-A
    New colliders were added to enhance the layout and gameplay. Existing colliders were modified to improve alignment and accuracy. These changes aim to refine the stage design and collision dynamics.

--- src/stages/level_1_a_stage.cpp
+++ src/stages/level_1_a_stage.cpp
@@ -32,6 +32,10 @@ namespace artifact
         colliders.emplace_back(-150 + block_scale * 28, 205 - block_scale * 2, block_scale * 2, block_scale, true);
         colliders.emplace_back(-150 + block_scale * 30, 205 - block_scale, block_scale * 14, block_scale, true);
         colliders.emplace_back(-150 + block_scale * 44, 205 - block_scale * 2, block_scale * 10, block_scale, true);
+        colliders.emplace_back(-150 + block_scale * 86.5, 205 - block_scale * 3, block_scale * 5, block_scale, true);
+        colliders.emplace_back(-150 + block_scale * 91.5, 205 - block_scale * 2, block_scale * 9, block_scale, true);
+        colliders.emplace_back(-150 + block_scale * 101.5, 205 - block_scale, block_scale * 3, block_scale, true);
+        colliders.emplace_back(-150 + block_scale * 118.5, 205 - block_scale * 2, block_scale * 9, block_scale, true);
 
         // Walls
         colliders.emplace_back(-150 - block_scale * 12.5, 205 - block_scale * 13, block_scale, block_scale * 10, true);
@@ -41,7 +45,16 @@ namespace artifact
         colliders.emplace_back(-150 + block_scale * 60, 205 - block_scale * 4, block_scale, block_scale * 8, true);
         colliders.emplace_back(-150 + block_scale * 67, 205 - block_scale * 7, block_scale, block_scale * 8, true);
         colliders.emplace_back(-150 + block_scale * 72.5, 205 - block_scale * 5, block_scale * 3, block_scale * 8, true);
-        colliders.emplace_back(-150 + block_scale * 75.5, 205 - block_scale * 4, block_scale * 3, block_scale * 8, true);
+        colliders.emplace_back(-150 + block_scale * 78.5, 205 - block_scale * 4, block_scale, block_scale * 8, true);
+        colliders.emplace_back(-150 + block_scale * 82.5, 205 - block_scale * 2, block_scale, block_scale * 8, true);
+        colliders.emplace_back(-150 + block_scale * 82.5, 205 - block_scale * 13, block_scale, block_scale * 6, true);
+        colliders.emplace_back(-150 + block_scale * 86.5, 205 - block_scale * 2, block_scale, block_scale * 8, true);
+        colliders.emplace_back(-150 + block_scale * 100.5, 205 - block_scale * 4, block_scale, block_scale * 3, true);
+        colliders.emplace_back(-150 + block_scale * 103.5, 205, block_scale, block_scale * 3, true);
+        colliders.emplace_back(-150 + block_scale * 105.5, 205 - block_scale * 6, block_scale, block_scale * 6, true);
+        colliders.emplace_back(-150 + block_scale * 111, 205 - block_scale * 2.5, block_scale, block_scale * 7, true);
+        colliders.emplace_back(-150 + block_scale * 111, 205 - block_scale * 13, block_scale, block_scale * 6, true);
+        colliders.emplace_back(-150 + block_scale * 127.5, 205 - block_scale * 9, block_scale, block_scale * 7, true);
 
         // Killnet
         colliders.emplace_back(-500, 500, block_scale * 150, block_scale, [&] { respawn(); });
