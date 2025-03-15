4b8d909ff8b1cbd2bf650419e08a8414e0204fc1
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-03-15 01:29:13 -0400

    Add templated add_component method to container_base
    Introduce a templated method for adding components by identifier, enabling more flexible and type-safe component creation. This enhances code reusability and simplifies component management.

--- include/ui/components/containers/container_base.h
+++ include/ui/components/containers/container_base.h
@@ -16,6 +16,14 @@ namespace artifact
         void draw() override;
         void update(int mouse_x, int mouse_y) override;
         virtual void add_component(ComponentBase *component);
+
+        template<typename T, typename... Args>
+        T* add_component(const char* identifier, Args&&... args) {
+            T* component = new T(identifier, std::forward<Args>(args)...);
+            this->components[component->get_identifier()] = component;
+            return component;
+        }
+
         virtual void remove_component(size_t index);
         virtual void remove_component(const char *identifier);
         virtual void remove_component(ComponentBase *component);
