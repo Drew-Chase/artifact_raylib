8e53f77b2737d61876f5a506e5258dc662b31f8c
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-03-15 01:29:57 -0400

    Add additional colliders to Level 1A stage layout
    Expanded ground and wall collision areas for Level 1A to improve gameplay boundaries and platform coverage. Updated killnet collider to use a lambda function for respawning behavior.

--- src/stages/level_1_a_stage.cpp
+++ src/stages/level_1_a_stage.cpp
@@ -22,16 +22,29 @@ namespace artifact
         float block_scale = 32 * 2.3;
 
         // Ground
+        colliders.emplace_back(-150 - block_scale * 12, 205 - block_scale * 4, block_scale * 11, block_scale, true);
         colliders.emplace_back(-150, 205, block_scale * 9, block_scale, true);
         colliders.emplace_back(-150 + block_scale * 9, 205 - block_scale, block_scale, block_scale, true);
         colliders.emplace_back(-150 + block_scale * 10, 205 - block_scale * 2, block_scale * 8, block_scale, true);
-
+        colliders.emplace_back(-150 + block_scale * 19, 205 - block_scale * 5, block_scale * 6, block_scale, true);
+        colliders.emplace_back(-150 + block_scale * 25, 205 - block_scale * 4, block_scale, block_scale, true);
+        colliders.emplace_back(-150 + block_scale * 26, 205 - block_scale * 3, block_scale * 2, block_scale, true);
+        colliders.emplace_back(-150 + block_scale * 28, 205 - block_scale * 2, block_scale * 2, block_scale, true);
+        colliders.emplace_back(-150 + block_scale * 30, 205 - block_scale, block_scale * 14, block_scale, true);
+        colliders.emplace_back(-150 + block_scale * 44, 205 - block_scale * 2, block_scale * 10, block_scale, true);
 
         // Walls
+        colliders.emplace_back(-150 - block_scale * 12.5, 205 - block_scale * 13, block_scale, block_scale * 10, true);
         colliders.emplace_back(-223.6, -90, block_scale, block_scale * 4, true);
+        colliders.emplace_back(-150 + block_scale * 18, 205 - block_scale * 5, block_scale, block_scale * 3, true);
+        colliders.emplace_back(-150 + block_scale * 53, 205 - block_scale, block_scale, block_scale * 10, true);
+        colliders.emplace_back(-150 + block_scale * 60, 205 - block_scale * 4, block_scale, block_scale * 8, true);
+        colliders.emplace_back(-150 + block_scale * 67, 205 - block_scale * 7, block_scale, block_scale * 8, true);
+        colliders.emplace_back(-150 + block_scale * 72.5, 205 - block_scale * 5, block_scale * 3, block_scale * 8, true);
+        colliders.emplace_back(-150 + block_scale * 75.5, 205 - block_scale * 4, block_scale * 3, block_scale * 8, true);
 
         // Killnet
-        colliders.emplace_back(-500, 500, block_scale * 150, block_scale, respawn);
+        colliders.emplace_back(-500, 500, block_scale * 150, block_scale, [&] { respawn(); });
     }
     void Level1AStage::draw() const
     {
