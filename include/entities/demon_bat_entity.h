ea03dd3a6327dd0132991ea365b82eb5b07cd042
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-03-23 20:14:43 -0400

    Add DemonBatEntity class implementation
    Introduce the DemonBatEntity class, derived from EnemyEntity, to represent a new enemy type. It manages its idle animation via a SpriteSheet and implements startup, update, draw, damage, kill, and destroy methods to handle its behavior and lifecycle.

---
+++ include/entities/demon_bat_entity.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "enemy_entity.h"
+#include "sprite_sheet.h"
+
+namespace artifact {
+class DemonBatEntity final : public EnemyEntity {
+    SpriteSheet* idle_sheet = nullptr;
+public:
+    void startup() override;
+    void destroy() override;
+    void draw() override;
+    void update(float deltaTime) override;
+    void damage(int damage) override;
+    void kill() override;
+};
+} // artifact
---
+++ src/entities/demon_bat_entity.cpp
@@ -0,0 +1,26 @@
+
+#include "entities/demon_bat_entity.h"
+
+namespace artifact
+{
+    void DemonBatEntity::startup()
+    {
+        EnemyEntity::startup();
+        idle_sheet = new SpriteSheet("game/texture/entities/demon_bat/idle%d.PNG", 7, 10);
+    }
+    void DemonBatEntity::draw()
+    {
+        EnemyEntity::draw();
+        if (idle_sheet != nullptr)
+            idle_sheet->draw(this->position, 2);
+    }
+    void DemonBatEntity::update(const float deltaTime)
+    {
+        EnemyEntity::update(deltaTime);
+        if (idle_sheet)
+            idle_sheet->update(deltaTime);
+    }
+    void DemonBatEntity::destroy() { EnemyEntity::destroy(); }
+    void DemonBatEntity::damage(const int damage) { EnemyEntity::damage(damage); }
+    void DemonBatEntity::kill() { EnemyEntity::kill(); }
+} // namespace artifact
