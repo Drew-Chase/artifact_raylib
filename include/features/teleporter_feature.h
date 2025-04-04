3b52f75d7533d2d2695f926c5f9b55925d11a90c
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-04-04 16:03:26 -0400

    Add teleporter feature with drawing and update logic
    This commit introduces the `TeleporterFeature` class, enabling teleportation functionality in the game. It includes position tracking, stage transitioning, collision detection, and rendering through a sprite sheet. The new feature is fully integrated with the game engine's stage and entity management systems.

--- include/features/teleporter_feature.h
+++ include/features/teleporter_feature.h
@@ -1,8 +1,26 @@
 #pragma once
+#include <memory>
+#include "collider.h"
+#include "stages/playable_stage.h"
+#include "stages/stages.h"
 
-namespace artifact {
-class TeleporterFeature {
+namespace artifact
+{
+    class TeleporterFeature
+    {
+        std::unique_ptr<Collider> collider;
+        Stages destination_stage = Stages::NONE;
+        Vector2 destination_position;
+        Vector2 position;
+        SpriteSheet *sheet;
 
-
-};
-} // artifact
+    public:
+        TeleporterFeature(Vector2 position, Vector2 destination_position, Stages destination_stage);
+        TeleporterFeature(const Vector2 position, const Vector2 destination_position) :
+            TeleporterFeature(position, destination_position, Stages::NONE) {}
+        TeleporterFeature(const Vector2 position, const Stages destination_stage) :
+            TeleporterFeature(position, {}, destination_stage) {}
+        void draw() const;
+        void update(float delta_time);
+    };
+} // namespace artifact
--- src/features/teleporter_feature.cpp
+++ src/features/teleporter_feature.cpp
@@ -1,5 +1,40 @@
 
 #include "features/teleporter_feature.h"
 
-namespace artifact {
-} // artifact
+#include "game.h"
+
+namespace artifact
+{
+    TeleporterFeature::TeleporterFeature(const Vector2 position, const Vector2 destination_position, const Stages destination_stage) :
+        destination_stage(destination_stage), destination_position(destination_position), position(position)
+    {
+        sheet = new SpriteSheet("game/texture/features/teleporter_%d.png", 9, 13);
+        collider = std::make_unique<Collider>(
+                position.x,
+                position.y,
+                40,
+                40,
+                [destination_stage, position](Entity *entity)
+                {
+                    if (  PlayerEntity *player = dynamic_cast<PlayerEntity *>(entity))
+                    {
+                        if (destination_stage != Stages::NONE)
+                            Game::get_instance()->get_stage_manager()->load_stage(destination_stage, position);
+                        else
+                        {
+                            player->set_position(position.x, position.y);
+                        }
+                    }
+                });
+    }
+    void TeleporterFeature::draw() const
+    {
+        if (sheet)
+            sheet->draw(position);
+    }
+    void TeleporterFeature::update(const float delta_time)
+    {
+        if (sheet)
+            sheet->update(delta_time);
+    }
+} // namespace artifact
