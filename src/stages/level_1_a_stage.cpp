0ad540fbed0c2cbbccf3d8c65e8753235cccd7c0
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-04-04 12:02:47 -0400

    Change player respawn to kill on collision
    Updated the collision behavior to immediately kill the player instead of triggering a respawn. This ensures consistent gameplay behavior when the player interacts with specific colliders.

--- src/stages/level_1_a_stage.cpp
+++ src/stages/level_1_a_stage.cpp
@@ -67,7 +67,7 @@ namespace artifact
 
         // Killnet
         colliders.emplace_back(-500, 500, block_scale * 150, block_scale, [&]
-                               { player->respawn(); });
+                               { player->kill(); });
     }
     Vector2 Level1AStage::get_spawn_position() const { return {0, 139}; }
     void Level1AStage::spawn_entities()
