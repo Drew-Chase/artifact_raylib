aa53a78e44f27a089ae218dab707ae5a128e3bc4
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-04-05 17:48:51 -0400

    Set size and handle dead state for DemonBatEntity
    Added width and height initialization to `startup` method to ensure consistent entity dimensions. Introduced checks in `draw` and `update` methods to properly skip actions when the entity is marked as dead, preventing unintended behavior or errors.

--- src/entities/demon_bat_entity.cpp
+++ src/entities/demon_bat_entity.cpp
@@ -6,16 +6,19 @@ namespace artifact
     void DemonBatEntity::startup()
     {
         EnemyEntity::startup();
+        width = height = 64;
         idle_sheet = new SpriteSheet("game/texture/entities/demon_bat/idle%d.PNG", 7, 10);
     }
     void DemonBatEntity::draw()
     {
+        if (is_dead())return;
         EnemyEntity::draw();
         if (idle_sheet != nullptr)
             idle_sheet->draw(this->position, 2);
     }
     void DemonBatEntity::update(const float delta_time)
     {
+        if (is_dead())return;
         EnemyEntity::update(delta_time);
         if (idle_sheet)
             idle_sheet->update(delta_time);
