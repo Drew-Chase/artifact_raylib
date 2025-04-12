82f0941a8fa86e096981856faef75dfe5ca5a2e3
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-04-11 23:24:44 -0400

    Change debug stage from LEVEL1A to LEVEL1B
    This updates the debug mode to load LEVEL1B instead of LEVEL1A. It ensures consistency with the current development testing requirements.

--- src/game.cpp
+++ src/game.cpp
@@ -83,7 +83,7 @@ namespace artifact
         game->display_settings->apply();
 
 #ifdef DEBUG
-        manager->load_stage(Stages::LEVEL1A);
+        manager->load_stage(Stages::LEVEL1B);
 #else
         manager->load_stage(Stages::TITLE_SCREEN);
 #endif
